9f13a85d22b5657f965bb2c71a7ec63271e1652c
Mdonks <137445961+Mdonks@users.noreply.github.com>
2024-03-21 15:48:25 -0600

    commit 1

---
+++ TiendaPC/TiendaPC/ViewModels/computadorasVM.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TuNombreDeEspacioDeTrabajo
+{
+    public class ComputadorasVM
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private const string BaseUrl = "https://apex.oracle.com/pls/apex/smeargle1628/api_tienda_pc";
+
+        public async Task<List<Computadora>> GetComputadorasAsync()
+        {
+            var response = await _client.GetAsync($"{BaseUrl}/computadoras");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var computadoras = JsonConvert.DeserializeObject<List<Computadora>>(content);
+                return computadoras;
+            }
+            else
+            {
+                return new List<Computadora>();
+            }
+        }
+
+        public async Task CrearComputadoraAsync(Computadora computadora)
+        {
+            var json = JsonConvert.SerializeObject(computadora);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync($"{BaseUrl}/computadoras", data);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Computadora creada exitosamente.");
+            }
+            else
+            {
+                Console.WriteLine("Error al crear la computadora:", response.StatusCode);
+            }
+        }
+
+        public async Task ActualizarComputadoraAsync(int id, Computadora computadora)
+        {
+            var json = JsonConvert.SerializeObject(computadora);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _client.PutAsync($"{BaseUrl}/computadoras/{id}", data);
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Computadora actualizada exitosamente.");
+            }
+            else
+            {
+                Console.WriteLine("Error al actualizar la computadora:", response.StatusCode);
+            }
+        }
+
+        public async Task EliminarComputadoraAsync(int id)
+        {
+            var response = await _client.DeleteAsync($"{BaseUrl}/computadoras/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Computadora eliminada exitosamente.");
+            }
+            else
+            {
+                Console.WriteLine("Error al eliminar la computadora:", response.StatusCode);
+            }
+        }
+    }
+
+    public class Computadora
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
+
---
+++ TiendaPC/TiendaPC/ViewModels/usuariosVM.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace TiendaPC.ViewModels
+{
+    public class UsuarioVM : INotifyPropertyChanged
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private const string BaseUrl = "https://apex.oracle.com/pls/apex/smeargle1628/api_tienda_pc/";
+
+        public UsuarioVM()
+        {
+            CrearUsuario = new Command(async () =>
+            {
+                string url = $"{BaseUrl}/usuarios";
+                var nuevoUsuario = new Usuario
+                {
+                    Nombre = Nombre,
+                    Direccion = Direccion,
+                    Correo = Correo
+                };
+
+                var json = JsonConvert.SerializeObject(nuevoUsuario);
+                var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await _client.PostAsync(url, data);
+                if (response.IsSuccessStatusCode)
+                {
+                    Result = "Usuario creado exitosamente.";
+                    // Actualizar lista de usuarios después de crear uno nuevo
+                    ObtenerUsuarios();
+                }
+                else
+                {
+                    Result = "Error al crear el usuario.";
+                }
+            });
+
+            // Obtener lista de usuarios al inicio
+            ObtenerUsuarios();
+        }
+
+        private async void ObtenerUsuarios()
+        {
+            try
+            {
+                HttpResponseMessage response = await _client.GetAsync($"{BaseUrl}/usuarios");
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    Usuarios = JsonConvert.DeserializeObject<List<Usuario>>(content);
+                }
+                else
+                {
+                    // Manejar error al obtener usuarios
+                }
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepción
+            }
+        }
+
+        private List<Usuario> _usuarios;
+        public List<Usuario> Usuarios
+        {
+            get => _usuarios;
+            set
+            {
+                _usuarios = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Usuarios)));
+            }
+        }
+
+        private string _nombre;
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                _nombre = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Nombre)));
+            }
+        }
+
+        private string _direccion;
+        public string Direccion
+        {
+            get => _direccion;
+            set
+            {
+                _direccion = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Direccion)));
+            }
+        }
+
+        private string _correo;
+        public string Correo
+        {
+            get => _correo;
+            set
+            {
+                _correo = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Correo)));
+            }
+        }
+
+        private string _result;
+        public string Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Result)));
+            }
+        }
+
+        public Command CrearUsuario { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Correo { get; set; }
+    }
+}
+
+
